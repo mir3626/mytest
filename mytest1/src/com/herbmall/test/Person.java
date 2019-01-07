72c017910ab1e22f0903b109802d481793c0dbd4
Tony <mir3626@gmail.com>
2019-01-07 17:18:46 +0900

    person class 수정

--- mytest1/src/com/herbmall/test/Person.java
+++ mytest1/src/com/herbmall/test/Person.java
@@ -21,7 +21,7 @@ public class Person {
 	}
 	
 	public void showInfo() {
-		System.out.println("Person클래스의 showInfo()메서드");
+		System.out.println("Person클래스의 showInfo()메서드 654321 GITHUB MASTER 테스트");
 	}
 	
 }
